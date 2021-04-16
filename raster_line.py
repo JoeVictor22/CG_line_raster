52d99b662673477045ba9a0f4bab0c5f5a82bb5b
joel <joelvictor1746@gmail.com>
2021-04-16 20:26:53 -0300

    idk

--- raster_line.py
+++ raster_line.py
@@ -18,11 +18,8 @@ matriz = np.zeros(res)
 dX = x2 - x1
 dY = y2 - y1
 
-if dY < dX:
-    m = dX/dY
-else:
-    m = dY/dX
-b = y1 - m*x1
+m = dY/dX
+b = y1 - (m*x1)
 
 '''
 if dX > dY:
@@ -32,22 +29,21 @@ else:
 '''
 
 def grava(x, y):
-    matriz[x][y] = 1
+    matriz[round(x)][round(y)] = 1
 
 
 grava(x1,y1)
 
-if dY < dX:
-
+if dX > dY:
     while(x1 < x2):
-        x1+=1
-        y = (x1-b)/m
-        grava(round(x1), round(y))
+        x1 += 1
+        y = m * x1 + b
+        grava(x1, y)
 else:
     while (y1 < y2):
         y1 += 1
         x = (y1-b)/m
-        grava(round(x), round(y1))
+        grava(x, y1)
 
 print(matriz.T[::-1])
 plt.matshow(matriz.T[::-1])
